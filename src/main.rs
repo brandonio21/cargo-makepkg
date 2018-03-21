dc9d0eda79c8361c2f291fd1455711899c868127
Brandon Milton <brandon.milton21@gmail.com>
2018-03-21 00:54:05 -0700

    Create directory

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,68 @@ extern crate serde_derive;
 extern crate serde_json;
 extern crate toml;
 
-use std::process::Command;
-use std::fs::File;
+use std::fs::{File, create_dir_all};
 use std::io::prelude::*;
-use std::io::Result;
-
-use serde_json::Value;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 mod config;
 use config::core::{CargoManifest, GeneratePackageConfig, PopulateFromCargoManifest};
 use config::arch::PKGBUILDConfig;
 
 fn get_cargo_manifest_path() -> String {
-    let output = Command::new("cargo").arg("locate-project").output().unwrap();
-    let json: Value = serde_json::from_str(&String::from_utf8(output.stdout).unwrap()).unwrap();
-    json["root"].as_str().unwrap().to_owned()
+    let output = Command::new("cargo").arg("locate-project").output()
+        .expect("Failed to call `cargo locate-project`");
+
+    let unicode_output = String::from_utf8(output.stdout)
+        .expect("Failed to convert output of `cargo locate-project` to unicode");
+
+    let json: serde_json::Value = serde_json::from_str(&unicode_output)
+        .expect("Failed to parse `cargo locate-project` output to JSON");
+
+    return json["root"].as_str()
+        .expect("Failed to convert `cargo locate-project` root path to str")
+        .to_owned();
 }
 
-fn read_cargo_manifest() -> Result<CargoManifest> {
-    let path = get_cargo_manifest_path();
-    let mut file = File::open(&path)?;
+fn read_cargo_manifest(manifest_path: &str) -> CargoManifest {
+    let mut file = File::open(manifest_path)
+        .expect(format!("Failed to open Cargo.toml at path {}", manifest_path).as_str());
+
     let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    Ok(CargoManifest::from_str(&contents))
+    file.read_to_string(&mut contents).expect("Failed to read Cargo.toml");
+    CargoManifest::from_str(&contents)
 }
 
-fn write_pkgbuild(pkgbuild: &PKGBUILDConfig) -> Result<()> {
-    let mut pkgbuild_file = File::create("PKGBUILD")?;
-    pkgbuild_file.write_all(pkgbuild.generate_config().as_bytes());
-    Ok(())
+fn get_pkgbuild_directory(
+    cargo_manifest_path: &str,
+    pkgbuild: &PKGBUILDConfig,
+) -> PathBuf {
+    let cargo_path = Path::new(cargo_manifest_path);
+    let root_dir = cargo_path.parent()
+        .expect(format!("Could not get parent of manifest path {}", cargo_manifest_path).as_str());
+    let version_str = format!("{}-{}", &pkgbuild.pkgver, &pkgbuild.pkgrel);
+    let pkgbuild_dir = root_dir.join("target").join(version_str);
+
+    // Finally, create the dir
+    if !pkgbuild_dir.exists() {
+        create_dir_all(&pkgbuild_dir).expect("Could not create pkgbuild dir");
+    }
+    pkgbuild_dir
+}
+
+fn write_pkgbuild(pkgbuild_dir: &Path, pkgbuild: &PKGBUILDConfig) {
+    let pkgbuild_file_path = pkgbuild_dir.join("PKGBUILD");
+    let mut pkgbuild_file = File::create(&pkgbuild_file_path)
+        .expect("Could not create PKGBUILD file");
+    pkgbuild_file.write_all(pkgbuild.generate_config().as_bytes())
+        .expect("Could not write PKGBUILD file");
 }
 
 fn main() {
-    let cargo_manifest = read_cargo_manifest().unwrap();
+    let cargo_manifest_path = get_cargo_manifest_path();
+    let cargo_manifest = read_cargo_manifest(&cargo_manifest_path);
     let pkgbuild_config = PKGBUILDConfig::from_cargo_manifest(cargo_manifest);
-    write_pkgbuild(&pkgbuild_config);
+    let pkgbuild_dir = get_pkgbuild_directory(&cargo_manifest_path, &pkgbuild_config);
+    write_pkgbuild(&pkgbuild_dir.as_path(), &pkgbuild_config);
 }
